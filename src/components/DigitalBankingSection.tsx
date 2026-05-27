435f0074dfea5130bef870736dd9a2348427d026
Neepun Patil <75531123+NeepunPatil57@users.noreply.github.com>
2026-05-27 16:36:37 +0530

    feat: adjust button gap in DigitalBankingSection for improved layout consistency

--- src/components/DigitalBankingSection.tsx
+++ src/components/DigitalBankingSection.tsx
@@ -253,7 +253,7 @@ export default function DigitalBankingSection() {
               N7 helps your financial institution improve the client experience,<br />automate and optimize procedures, simplify banking operations.
             </p>
           </div>
-          <div className="relative z-10 flex flex-col lg:flex-row gap-4 lg:ml-auto items-center w-full lg:w-auto">
+          <div className="relative z-10 flex flex-col lg:flex-row gap-8 lg:ml-auto items-center w-full lg:w-auto">
             <Button variant="outline" className="w-[240px] lg:w-auto text-center">Contact Us</Button>
             <Button variant="primary" className="w-[240px] lg:w-auto text-center">Request Demo</Button>
           </div>
